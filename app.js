d128f2a35047fa0197d03384da411705a0a35605
Nirav Trivedi <niravtrivedi2112@gmail.com>
2018-12-01 14:38:09 +0530

    file upload

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var authenticate = require('./authenticate');
 
 const mongoose = require('mongoose');
 
+
+
 // const Dishes = require('./models/dishes');
 
 // const url = 'mongodb://localhost:27017/conFusion';
@@ -30,6 +32,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promotionRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 
 var app = express();
@@ -96,6 +99,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 
 
